5abd9a2d9938c71cca1519ad7a396ba7af509b69
EcheCostabel <exequielcostabel@gmail.com>
2023-04-06 13:07:38 -0300

    adding types

--- src/types/types.js
+++ src/types/types.js
@@ -4,9 +4,19 @@
         uiOpenModal: '[ui] Open modal',
         uiCloseModal: '[ui] Close modal',
 
+        
         eventSetActive: '[event]  Set active ',
         eventAddNew: '[event]  Add new ',
         eventClearActveEvent: '[event] Clear event',
         eventUpdate: '[event] Event updated',
-        eventDeleted: '[event] Event deleted'
+        eventDeleted: '[event] Event deleted',
+
+
+        authChecking: '[auth] Checking login state',
+        authCheckingFinish: '[auth] Finish checking login state',
+        authStartFinish: '[auth] Start login',
+        authLogin: '[auth] Login',
+        authStartRegister: '[auth] Start register',
+        authStartStartTokenRenew: '[auth] Start token renew',
+        authLogout: '[auth] Logout',
     }

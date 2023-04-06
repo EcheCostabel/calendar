65e9685a468ac58815537f4b1d510bc12f5a90fe
EcheCostabel <exequielcostabel@gmail.com>
2023-04-06 13:00:07 -0300

    set .env variables

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { CalendarApp } from './CalendarApp';
 import './components/styles.css';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  

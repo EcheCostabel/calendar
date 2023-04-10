1d92ff3aaa092300e7fbc6e070a628aa93277515
EcheCostabel <exequielcostabel@gmail.com>
2023-04-10 18:27:38 -0300

    adding startRegister

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 import { fetchWithoutToken } from "../helpers/fetch";
 import { types } from "../types/types";
 
@@ -17,6 +18,8 @@ export const startLogin = (email, password) => {
                 uid: body.uid,
                 name: body.name
             }))
+        } else {
+            Swal.fire('Error', body.msg, 'error')
         }
     }
 };
@@ -28,3 +31,24 @@ const login = (user) => {
         payload: user
     }
 };
+
+
+
+export const startRegister = (email, password, name) => {
+    return async(dispatch) => {
+        const resp = await fetchWithoutToken('auth/new', { email, password, name}, 'POST');
+        const body = await resp.json();
+
+        if(body.ok) {
+            localStorage.setItem('token', body.token);  //si el usuario ingresa me va a guardar el 'token' que esta en body.token
+            localStorage.setItem('token-init-date', new Date().getTime());
+
+            dispatch(login({
+                uid: body.uid,
+                name: body.name
+            }))
+        } else {
+            Swal.fire('Error', body.msg, 'error')
+        }
+    }
+}
